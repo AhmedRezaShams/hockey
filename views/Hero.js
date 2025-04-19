656efcbc9429977791ee64166b77bd65ee9fc30a
Ahmed Reza <ahmedrezashams4@gmail.com>
2025-04-19 09:50:27 +0600

    working

--- views/Hero.js
+++ views/Hero.js
@@ -9,20 +9,26 @@ const images = [
 
 function Hero() {
     return (
-        <div className=''>
-            <div className='relative flex'>
+        <div className="py-4">
+            <div className="relative flex">
+                {/* Carousel with relative wrapper */}
                 <ImageCarousel images={images} />
-            </div>
-            <div className='obsolute inset-0'>
-                <div className='w-[566] h-[165] bg-[#131318]'>
-                    <div>
-                        <h1 className='font-extrabold text-2xl text-white'>Meet all the heroes from the field</h1>
-                        <p className='font-medium text-lg text-white opacity-60'>Dit amet consectetur. Condimentum dignissim adipiscing aliquam turpis placerat dolor. Purus urna in sit nullam proin. </p>
+
+                {/* Overlay inside the same relative wrapper */}
+                <div className="absolute inset-0 flex items-end justify-end z-10 bg-black/50 px-3 py">
+                    <div className="w-[566px] h-[165px] bg-[#131318] rounded-2xl p-6 text-center">
+                        <h1 className="font-extrabold text-2xl text-white mb-2">
+                            Meet all the heroes from the field
+                        </h1>
+                        <p className="font-medium text-lg text-white opacity-60">
+                            Dit amet consectetur. Condimentum dignissim adipiscing aliquam
+                            turpis placerat dolor. Purus urna in sit nullam proin.
+                        </p>
                     </div>
                 </div>
             </div>
-
         </div>
+
 
     )
 }
